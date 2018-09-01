a34b5f9f3cb95aa528e0d936b0cdc38a5f660f54
Esteban <esteban@gmail.com>
2018-09-01 10:00:54 -0500

    dojoVue To do list

---
+++ script.js
@@ -0,0 +1,55 @@
+let taskApp = new Vue({
+    el: '#taskApp',
+    data: {
+        tasks: [
+
+        ]
+    },
+
+    mounted(){
+        this.$http.get("http://142.93.202.10:8080/tasks").then(result =>{
+            this.tasks = result.data;
+        },error =>{
+            console.error(error);
+        })
+    },
+    
+    methods: {
+        deleteTask: function(task){
+            this.$http.delete("http://142.93.202.10:8080/tasks", {bosy:{id: task.id}}).then(result => {
+                console.log(JSON.stringify(result.data));
+                this.tasks.splice(this.tasks.indexOf(task),1);
+            },error => {
+                console.error(error);
+            })
+            
+        },
+
+        addTask: function(e){
+            e.preventDefault();
+            if(this.tasks.name){
+                let task = {
+                    name: this.tasks.name, 
+                    done: false
+                }
+                this.$http.post("http://142.93.202.10:8080/tasks",task).then(result => {
+                    this.tasks.push(result.data);
+                    console.log(this.task);
+                },error => {
+                    console.error(error);
+                })
+                this.tasks.name ="";
+            }
+        },
+
+        updateTask: function(task){
+            this.$http.put("http://142.93.202.10:8080/tasks", task).then(result => {
+                console.log(JSON.stringify(result.data));
+                this.tasks = result.data;
+            }, error => {
+                console.error(error);
+            })
+        }
+        
+    }
+});
